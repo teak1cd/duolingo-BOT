e23d116dd929dd58db512fa2f2148e55d3a7394e
teak1cd <isenne@sterneschool.org>
2016-10-21 14:16:37 -0700

    Update bot3.js

--- bot3.js
+++ bot3.js
@@ -10,16 +10,62 @@ var exeptions = [
 ];
 function f(){
 try{
+    window.simulateClick = function(control)
+{
+    if (document.all)
+    {
+        control.click();
+    }
+    else
+    {
+        var evObj = document.createEvent('MouseEvents');
+        evObj.initMouseEvent('click', true, true, window, 1, 12, 345, 7, 220, false, false, true, false, 0, null );
+        control.dispatchEvent(evObj);
+    }
+}
 window.g = function(){        
     window.requestAnimationFrame(window.g);
     try{
         if(document.getElementsByClassName("player")[2].innerHTML==="Translate this text"){
             window.l();
+            var e = new Event("keydown");
+            e.key="a";    // just enter the char you want to send 
+            e.keyCode=e.key.charCodeAt(0);
+            e.which=e.keyCode;
+            e.altKey=false;
+            e.ctrlKey=true;
+            e.shiftKey=false;
+            e.metaKey=false;
+            e.bubbles=true;
+            document.dispatchEvent(e);
+            simulateClick(document.getElementById("next_button"));
+            simulateClick(document.getElementById("next_button"));
         }else{
             var k = document.getElementsByClassName("player")[2].innerHTML.split("“");
             k[1] = k[1].substring(0, k[1].length - 1);
             var t = window.l(k[1]).replace(/ ;/g,"");
             document.getElementsByClassName("player")[2].innerHTML = t;
+            var _h = document.getElementsByClassName("title");
+            var c = [0,0,0];
+            try{
+            for(var i = 0;i<3;i++){
+                for(var j = 0;j<_h[i].innerHTML.length;j++){
+                    if(_h[i].innerHTML[j]===t[i][j]){
+                        c[i]++;
+                    }
+                }
+            }
+            }catch(e){
+            }
+            var s = 0;
+            var __g = 0;
+            if(c[0] > s){ s = c[0];__g=1;}
+            if(c[1] > s){ s = c[1];__g=2;}
+            if(c[2] > s){ s = c[2];__g=3;}
+            console.log(__g);
+            simulateClick(document.getElementById("option-"+__g));
+            simulateClick(document.getElementById("next_button"));
+            simulateClick(document.getElementById("next_button"));
         }
     }catch(e){}
 }
