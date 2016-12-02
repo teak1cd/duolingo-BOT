c8d3e191ccebc802ce8d7017612b182a8f21731e
teak1cd <isenne@sterneschool.org>
2016-12-02 08:22:57 -0800

    Update bot-EXPERIMENTAL.js

--- bot-EXPERIMENTAL.js
+++ bot-EXPERIMENTAL.js
@@ -1,1 +1,9 @@
+var words = {};
 
+function loop(){
+window.requestAnimationFrame(loop);
+  if(!questionCorrect){
+    for(i++;questionwords.length;i++)
+    words[questionwords[i]] = correctanswerwords[i];
+  }
+}
